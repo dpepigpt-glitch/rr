4c99d9d593d28475c3dac8895235caa78e1214d4
dpepigpt-glitch <dpepigpt@gmail.com>
2026-04-17 00:13:55 -0300

    chore: remove config.example.js (not needed with hash approach)

--- config.example.js
+++
@@ -1,7 +0,0 @@
-// Copie este arquivo para config.js e defina as senhas reais.
-// O arquivo config.js está no .gitignore e NÃO será enviado ao repositório.
-var CONFIG = {
-  pw: 'SENHA_ADMIN_AQUI',   // senha geral (Histórico, encerrar jornada, jornadas itinerantes)
-  pw1: 'SENHA_DEF1_AQUI',  // senha da 1ª Defensoria Pública Itinerante
-  pw2: 'SENHA_DEF2_AQUI'   // senha da 2ª Defensoria Pública Itinerante
-};
